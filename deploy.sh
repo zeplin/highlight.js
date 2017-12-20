a81c4ed662b0a4ac2f9704e79b455b846ae550eb
Berk Çebi <berk@zeplin.io>
2017-12-20 14:07:38 +0300

    Move build command to script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ BUILD_DIR="build"
 S3_DIR="s3://zeplin-packages-prod/highlight.js"
 VERSION=$(git describe --tags --exact-match)
 
+node tools/build.js swift objectivec xml css scss less stylus javascript -t cdn
 sudo aws s3 cp $BUILD_DIR/ $S3_DIR/$VERSION/ --recursive
