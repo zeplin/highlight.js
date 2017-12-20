409bc6964f5047ff24ad9393e32d60649ba7e1a7
Berk Çebi <berk@zeplin.io>
2017-12-20 14:45:37 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ S3_DIR="s3://zeplin-packages-prod/highlight.js"
 VERSION=$(git describe --tags --exact-match)
 
 node tools/build.js swift objectivec xml css scss less stylus javascript -t cdn
-sudo aws s3 cp $BUILD_DIR/ $S3_DIR/$VERSION/ --recursive
+sudo aws s3 cp $BUILD_DIR/ $S3_DIR/$VERSION/ --recursive --acl public-read
