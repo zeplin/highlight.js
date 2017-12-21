ead4ddf145993265164d21cd4a0ff5a00b53636f
Berk Çebi <berk@zeplin.io>
2017-12-21 11:04:46 +0300

    Remove directory

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -e
 
 BUILD_DIR="build"
-S3_DIR="s3://zeplin-packages-prod/highlight.js"
 VERSION=$(git describe --tags --exact-match)
 
 node tools/build.js swift objectivec xml css scss less stylus javascript -t cdn
